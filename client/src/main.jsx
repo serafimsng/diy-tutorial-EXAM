efa0456d041e356b74ae014bb979c1d59a620dcd
Zacharia Ananta Wijaya <kachenchaney@gmail.com>
2026-01-26 15:04:00 +0700

    Pasang LogRocket di main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// --- 1. PANGGIL LOGROCKET (WAJIB ADA) ---
+import LogRocket from 'logrocket';
+
+// --- 2. AKTIFKAN DENGAN ID KAMU ---
+LogRocket.init('acuxic/toko-zacharia'); 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
